1fda645bc9c2d0bea7b09b3131d422bf87c4ba5e
Michael <huakatteer@gmail.com>
2024-10-31 16:39:41 +0200

    post generation done, need to fix image scaling

--- use_me.js
+++ use_me.js
@@ -14,13 +14,41 @@ function fetchJSONData() {
 function createPost(data){
     let article = document.createElement("article");
     article.className = "post";
+    document.getElementById("all_posts").appendChild(article)
+
     let header = document.createElement("header");
     header.className = "post-header";
+    article.appendChild(header);
+
     let prof_pic = document.createElement("img");
     prof_pic.className = "profile-icon";
-    prof_pic.src = data.user.profile_picture;
+    prof_pic.src = "images/prof_pics/" + data.user.profile_picture;
+    prof_pic.alt = "User Icon"
+    header.appendChild(prof_pic);
 
-    document.getElementById("all_posts").appendChild(article)
+    let date = document.createElement("h4");
+    date.className = "post-date";
+    date.textContent = data.date;
+    header.appendChild(date);
+
+    if (data.image != null){
+        let post_pic = document.createElement("img");
+        post_pic.src = "images/post_pics/" + data.image;
+        post_pic.alt = "Tartu 2022";
+        post_pic.className = "post-image";
+        article.appendChild(post_pic);
+    }
+
+    let post_text = document.createElement("p");
+    post_text.textContent =data.text;
+    post_text.className = "post-text";
+    article.appendChild(post_text);
+
+    let like = document.createElement("img");
+    like.className = "like-icon";
+    like.src = "images/thumb_up_24dp_434343_FILL0_wght400_GRAD0_opsz24.svg"
+    like.alt = "Like Icon";
+    article.appendChild(like);
 }
 
 function addPosts(json){
@@ -28,16 +56,17 @@ function addPosts(json){
     console.log(posts)
     const nr_of_posts = Object.keys(posts.posts).length
     console.log(nr_of_posts)
-    console.log(posts.posts[1].user.profile_picture)
-    //TODO: create loop for all posts
-    //document.getElementById("all_posts").innerHTML = ""
+
+    for (let i = 0; i < nr_of_posts; i++) {
+        createPost(posts.posts[i])
+    }
 }
 
 function fetchOnlineData(){
     let req = new XMLHttpRequest();
 
     req.onreadystatechange = () => {
-        if (req.readyState == XMLHttpRequest.DONE) {
+        if (req.readyState === XMLHttpRequest.DONE) {
             addPosts(req.responseText)
         }
     };
