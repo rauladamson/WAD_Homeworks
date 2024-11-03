020e79dfe59289f8bfcb4d239ef7035b4b5e94d0
Michael <huakatteer@gmail.com>
2024-11-03 17:42:57 +0200

    attempted to fix image scaling

--- scripts/use_me.js
+++ scripts/use_me.js
@@ -31,24 +31,29 @@ function createPost(data){
     date.textContent = data.date;
     header.appendChild(date);
 
+    let div = document.createElement("div");
+    div.className = "post-content";
+    article.appendChild(div);
+
     if (data.image != null){
         let post_pic = document.createElement("img");
+        post_pic.className = "post-pic"
         post_pic.src = "images/post_pics/" + data.image;
         post_pic.alt = "Tartu 2022";
         post_pic.className = "post-image";
-        article.appendChild(post_pic);
+        div.appendChild(post_pic);
     }
 
     let post_text = document.createElement("p");
     post_text.textContent =data.text;
     post_text.className = "post-text";
-    article.appendChild(post_text);
+    div.appendChild(post_text);
 
     let like = document.createElement("img");
     like.className = "like-icon";
     like.src = "images/thumb_up_24dp_434343_FILL0_wght400_GRAD0_opsz24.svg"
     like.alt = "Like Icon";
-    article.appendChild(like);
+    div.appendChild(like);
 }
 
 function addPosts(json){
@@ -71,7 +76,7 @@ function fetchOnlineData(){
         }
     };
 
-    req.open("GET", "https://api.jsonbin.io/v3/b/67237c12ad19ca34f8c1a220/latest", true);
+    req.open("GET", "https://api.jsonbin.io/v3/b/67279687acd3cb34a8a1dad2/latest", true);
     req.setRequestHeader("X-Master-Key", "$2a$10$H/9wkZrAnGEOWUYY3hH39.JsPFRkfBk.F.ZUNM8dFtccZtpgpagNK");
     req.setRequestHeader("X-Bin-Meta", "false")
     req.send();}
