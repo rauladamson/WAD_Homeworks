7a488d9fd229ad5ec2e262dcf4fba786b33f078b
Tristan Laur <144528378+LgndryPotato@users.noreply.github.com>
2024-11-24 18:58:42 +0200

    Created Footer.vue and added the needed code

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<template>
+  <footer>
+    <p>© 2024 AQ. All contents of this website are covered by the GNU General Public License GPL-3.0.</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: #f8f9fa;
+  padding: 1rem;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style>
