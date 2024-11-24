ee4e14a2706258f6f4c4be3658115264ab58788f
Tristan Laur <144528378+LgndryPotato@users.noreply.github.com>
2024-11-24 18:58:13 +0200

    Created Header.vue and added the needed code

---
+++ Homework_3/src/components/Header.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<template>
+  <header>
+    <nav>
+      <div id="links">
+        <router-link to="/">Main Page</router-link>
+        <router-link to="/signup">Signup</router-link>
+      </div>
+    </nav>
+  </header>
+</template>
