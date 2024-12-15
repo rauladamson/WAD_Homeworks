cb8c61dd28b0f4705fc65a6a6a80e4c29690e7e2
Michael <huakatteer@gmail.com>
2024-12-15 21:46:26 +0200

    something

--- Homework_3/database.js
+++ Homework_3/database.js
@@ -2,7 +2,7 @@
 import pg from 'pg'
 const { Pool } = pg
 const pool = new Pool({
-    user: "postgres",
+    user: "testuser",
     password: "isherenow",
     database: "testWad",
     host: "localhost",
